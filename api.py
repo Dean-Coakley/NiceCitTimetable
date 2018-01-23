9d371339b67d1c9aa1d2ebc8f00cc6c7e7e42dfc
Dean Coakley <dean@Deans-MacBook-Pro.local>
2018-01-23 12:05:44 +0000

    rm junk files

--- api.py
+++ api.py
@@ -1,18 +1,18 @@
+import json
 from flask import Flask, render_template
-from flask_restful import Resource, Api
+from flask_restful import Api#, Resource
 from timetable import get_timetable
-import json 
 
-app = Flask(__name__)
-api = Api(app) 
+APP = Flask(__name__)
+API = Api(APP)
 
-@app.route("/")
+@APP.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/show_timetable/<course_id>", methods=["POST"])
+@APP.route("/show_timetable/<course_id>", methods=["POST"])
 def show_timetable(course_id):
     return json.dumps(get_timetable(course_id), indent=4)
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=80, debug=True)
+    APP.run(host='localhost', port=80, debug=True)
